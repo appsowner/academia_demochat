b2f06d063e2dae0d70b5690c642ccb6a125ad6d8
AO AppsOwner SPA <110705397+appsowner@users.noreply.github.com>
2025-02-12 21:08:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-  },
   preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-    allowedHosts: ['*'],
-  },
-  build: {
-    outDir: 'dist', // Asegura que Vite genere los archivos correctamente
+    host: '0.0.0.0',  // Permite acceso externo
+    port: process.env.PORT || 3000, // Usa el puerto asignado por Render
+    allowedHosts: ['academia-demochat.onrender.com'], // Agrega tu dominio de Render
   },
 });
